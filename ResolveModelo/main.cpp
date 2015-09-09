234d4616f1eb3a53775cbdd782761b460dfd4aa9
Italo Silva <italo@maleldil.com>
2015-09-09 18:43:52 -0300

    Adiciona função que gera um relatório em HTML, mas está bugada

--- ResolveModelo/main.cpp
+++ ResolveModelo/main.cpp
@@ -98,6 +98,58 @@ std::tuple<CursoPtr, std::string, int> menu() {
 	}
 }
 
+
+bool escreveHTML(Curso const* curso, const std::vector<int>& solucao,
+				 std::string caminho, std::string nomeAluno) {
+	std::ofstream saida(caminho + "\\" + nomeAluno + ".html");
+
+	if (!saida.is_open())
+		return false;
+
+	const auto& horario = curso->horarios();
+	const auto& nomeDisciplinas = curso->nomeDisciplinas();
+	const auto numDisciplinas = curso->creditos().size();
+	const auto numHorarios = horario.size();
+
+	saida << "<!DOCTYPE html>\n"
+		<< "<html>\n"
+		<< "<body>\n"
+		<< "<table style=\"width:100%\">\n";
+
+	saida << "<tr>\n"
+		<< "<th>Segunda</th>\n"
+		<< "<th>Terça</th>\n"
+		<< "<th>Quarta</th>\n"
+		<< "<th>Quinta</th>\n"
+		<< "<th>Sexta</th>\n"
+		<< "</tr>\n";
+
+	for (auto i = 0; i < numHorarios / 5; i++) {
+		saida << "<tr>\n";
+		for (auto j = 0; j <= numHorarios - 4; j += 4) {
+			auto encontrou = false;
+			for (auto k = 0; k < numDisciplinas; k++) {
+				if (horario[j + i][k] && solucao[k]) {
+					saida << "<td>" << nomeDisciplinas[k] << "</td>\n";
+					encontrou = true;
+					break;
+				}
+			}
+			if (!encontrou) {
+				saida << "<td> ----- </td>\n";
+			}
+		}
+		saida << "</tr>\n";
+	}
+
+	saida << "</table>\n"
+		<< "</body>\n"
+		<< "</html>\n";
+
+	return true;
+}
+
+
 bool geraAlunos(std::string caminho, CursoPtr curso, int numAlunos) {
 	// Cria o diretório, e encerra a função se for malsucedido
 	if (!CreateDirectory(caminho.c_str(), nullptr))
@@ -118,6 +170,7 @@ bool geraAlunos(std::string caminho, CursoPtr curso, int numAlunos) {
 		copy(begin(nomeDisciplinas), end(nomeDisciplinas),
 		     std::ostream_iterator<std::string>(saida, " "));
 		saida << solver.valorFinal() << "\n\n";
+		escreveHTML(curso.get(), solver.solucao(), caminho, aln);
 	}
 
 	// Cria a stream de escrita do arquivo

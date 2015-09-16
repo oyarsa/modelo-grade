82443a6cedd300acf84e3ce5301809e94b280fac
Italo Silva <italo@maleldil.com>
2015-09-16 18:14:59 -0300

    Modifica construtor de mersenne twister para descartar alguns números (warm-up)

--- InstanciasAleatorias/aleatorio.cpp
+++ InstanciasAleatorias/aleatorio.cpp
@@ -8,7 +8,9 @@ Aleatorio::Aleatorio()
 	: dist{0, MAX_RANDOM}, 
 	seed{static_cast<TipoSeed>(
 		TipoRelogio::now().time_since_epoch().count())},
-	gerador{seed} {}
+	gerador{seed} {
+	gerador.discard(gerador.state_size);
+}
 
 std::mt19937 Aleatorio::geradorAleatorio() {
 	return gerador;
